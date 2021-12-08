af3ab888c6b2d72afa42028f9aab7979efdddc59
Arham Jain <arhamjain@gmail.com>
2021-12-08 15:16:27 -0500

    Updated wordcount to use a big block of memory

--- wordcount/count.c
+++ wordcount/count.c
@@ -17,7 +17,7 @@ uint64_t secret = 7;
 uint64_t hash(char* buffer){
   return wyhash(buffer, strlen(buffer), 0, &secret) % buckets;
 }
-void indirect_access(char** words, int word_count) {
+void indirect_access(char words[][10], int word_count) {
   for (int i = 0; i < word_count; i++){
     uint64_t bucket_index = hash(words[i]);
     struct Entry* bucket = &counts[bucket_index];
@@ -25,23 +25,19 @@ void indirect_access(char** words, int word_count) {
       strcpy(bucket->word, words[i]);
     }
     bucket->count++;
-    free(words[i]);
   }
 }
 
 
 int main(int argc, char *argv[]){
   int max_lines = 10000000;
-  char **words = (char **)malloc(sizeof(char*)*max_lines);
+  char (*words)[10] = malloc(max_lines * 10);
   FILE *file = fopen(argv[1], "r");
   int chars_read;
-  char** line = &words[0];
   size_t len;
   int line_no = 0;
-  while ((chars_read = getline(line, &len, file)) != -1) {
+  while(fgets(words[line_no], 10, file) && line_no < max_lines) {
     line_no++;
-    (*line)[strlen(*line)-1] = '\0';
-    line = &words[line_no];
   }
   clock_t begin = clock();
   indirect_access(words, line_no);
@@ -50,9 +46,10 @@ int main(int argc, char *argv[]){
   printf("time_spent : ~ %fsec\n", time_spent);
   for (int i = 0; i < buckets; i++){
     if(counts[i].count > 0){
-      printf("%s %d\n", counts[i].word, counts[i].count);
+      printf("%s %lu\n", counts[i].word, counts[i].count);
     }
   }
+  free(words);
   fclose(file);
   return 0;
 }
